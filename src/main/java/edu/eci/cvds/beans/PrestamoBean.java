ac1b3c1e7a787a1b8c7ffc913b5d359cdea8568a
unknown <2127790@labinfo.is.escuelaing.edu.co>
2019-11-15 14:34:23 -0500

    iniciamos el desarrollo del prestamo bean

--- src/main/java/edu/eci/cvds/beans/PrestamoBean.java
+++ src/main/java/edu/eci/cvds/beans/PrestamoBean.java
@@ -13,6 +13,8 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Date;
+import edu.eci.cvds.samples.services.ServiciosBiblioteca;
 /**
  *
  * @author 2LateTeam
@@ -21,5 +23,44 @@ import java.util.List;
 @ManagedBean(name = "prestamoBean")
 @ViewScoped
 public class PrestamoBean implements Serializable {
-        
+        private ServiciosBiblioteca serviciosBiblioteca;
+
+        private String correoUsuario;
+        private String IdRecurso;
+        private Date horaInicio;
+        private int duracion;
+
+
+    public String getCorreoUsuario() {
+        return correoUsuario;
+    }
+
+    public void setCorreoUsuario(String correoUsuario) {
+        this.correoUsuario = correoUsuario;
+    }
+
+    public String getIdRecurso() {
+        return IdRecurso;
+    }
+
+    public void setIdRecurso(String idRecurso) {
+        IdRecurso = idRecurso;
+    }
+
+    public Date getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(Date horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
 }
